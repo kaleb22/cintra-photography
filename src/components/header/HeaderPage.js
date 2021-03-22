18fce226f113e4018cdcb61cf6da000d6562dbba
kaleb22 <kaleb22@hotmail.com>
2021-03-22 13:55:12 -0300

    feat: BEM pattern for header css

--- src/components/header/HeaderPage.js
+++ src/components/header/HeaderPage.js
@@ -5,7 +5,7 @@ const HeaderPage = () => {
     return (
         <header className="app-header">
             <span className="logo">Raphaela Cintra Fotografia</span>
-            <div className="app-header__right">
+            <div className="app-header--right">
                 <ul className="list-unstyled">
                     <li><a href="https://facebook.com" className="fa fa-facebook"></a></li>
                     <li><a href="https://instagram.com" className="fa fa-instagram"></a></li>
