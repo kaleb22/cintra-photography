8e233f6821d2417d5b46689a6539811d067eafe8
kaleb22 <kaleb22@hotmail.com>
2021-03-22 14:15:10 -0300

    refac to implement BEM

--- src/components/header/HeaderPage.js
+++ src/components/header/HeaderPage.js
@@ -6,7 +6,7 @@ const HeaderPage = () => {
         <header className="app-header">
             <span className="logo">Raphaela Cintra Fotografia</span>
             <div className="app-header--right">
-                <ul className="list-unstyled">
+                <ul className="list list--unstyled">
                     <li><a href="https://facebook.com" className="fa fa-facebook"></a></li>
                     <li><a href="https://instagram.com" className="fa fa-instagram"></a></li>
                 </ul>
