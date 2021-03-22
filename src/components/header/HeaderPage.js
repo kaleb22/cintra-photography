0f1797f95ca0731ea18c9b0bdf20f7805fc9c13f
kaleb22 <kaleb22@hotmail.com>
2021-03-22 13:22:14 -0300

    feat: implementing header component

---
+++ src/components/header/HeaderPage.js
@@ -0,0 +1,18 @@
+import './HeaderPage.scss'
+
+const HeaderPage = () => {
+
+    return (
+        <header className="app-header">
+            <span className="logo">Raphaela Cintra Fotografia</span>
+            <div className="app-header__right">
+                <ul className="list-unstyled">
+                    <li><a href="https://facebook.com" className="fa fa-facebook"></a></li>
+                    <li><a href="https://instagram.com" className="fa fa-instagram"></a></li>
+                </ul>
+            </div>
+      </header>
+    )
+}
+
+export default HeaderPage;
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeaderPage from '../components/header/HeaderPage'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <HeaderPage></HeaderPage>
+    </div>
+  );
+}
+
+export default App;
