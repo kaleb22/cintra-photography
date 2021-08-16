9e1c4af2e877d63bee0cdfb97bce8e1ad6bfd061
kaleb22 <kaleb22@hotmail.com>
2021-08-16 18:51:54 -0300

    fix: adjusting height from scroll component

--- src/components/scroll/Scroll.js
+++ src/components/scroll/Scroll.js
@@ -3,7 +3,7 @@ import './Scroll.scss'
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', height:'32em'}} className="hide-scrollbar">
+        <div style={{ overflow: 'scroll', height:'55em'}} className="hide-scrollbar">
             {props.children}
         </div>
     );
