e2ae4b8913146189aca91b40ed9e5c71c6ccab9d
kaleb22 <kaleb22@hotmail.com>
2021-08-18 13:00:11 -0300

    feat: implementing submit function to send form

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import InputMask from 'react-input-mask';
+import emailjs from 'emailjs-com';
+
 import './FormComponent.scss'
 
 const FormComponent = () => {
 
+  function sendEmail(e) {
+    debugger;
+    console.log('service id ==> ', process.env.REACT_APP_SERVICE_ID);
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,
+                     process.env.REACT_APP_TEMPLATE_ID,
+                     e.target,
+                     process.env.REACT_APP_USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return(
       <div className="form-container">
-        <Form>
+        <Form onSubmit={sendEmail} >
+        <FormGroup className="field-size">
+            <Label for="nomeField" className="field-title">Nome:</Label>
+            <Input type="text" name="nome_cliente" id="nomeField" placeholder="nome completo" />
+          </FormGroup>
           <FormGroup className="field-size">
             <Label for="emailField" className="field-title">E-mail:</Label>
-            <Input type="email" name="email" id="emailField" placeholder="insira seu email" />
+            <Input type="email" name="email_cliente" id="emailField" placeholder="insira seu email" />
           </FormGroup>
           <FormGroup className="field-size">
             <Label for="celField" className="field-title">Celular:</Label>
             <Input 
               type="cel" 
-              name="celular" 
+              name="cel_cliente" 
               id="celField" 
               mask="(99) 99999-9999" 
               maskChar=" " 
@@ -25,9 +47,9 @@ const FormComponent = () => {
           </FormGroup>
           <FormGroup>
             <Label for="msgField" className="field-title">Mensagem:</Label>
-            <Input type="textarea" name="mensagem" id="msgField" maxLength="350" />
+            <Input type="textarea" name="mensagem_cliente" id="msgField" maxLength="350" />
           </FormGroup>
-          <Button style={{ marginTop: '2em'}}>Enviar</Button>
+          <Button type="submit" style={{ marginTop: '2em'}}>Enviar</Button>
         </Form>
       </div>
 
