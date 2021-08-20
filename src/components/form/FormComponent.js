edb07803629599fe04c5da38be39106e7dac6ae1
kaleb22 <kaleb22@hotmail.com>
2021-08-20 13:19:29 -0300

    feat: implementing field validation on form fields

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -1,15 +1,79 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
 import InputMask from 'react-input-mask';
 import emailjs from 'emailjs-com';
 
 import './FormComponent.scss'
 
-const FormComponent = () => {
+class FormComponent extends Component {
 
-  function sendEmail(e) {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      clientName: '',
+      clientEmail: '',
+      clientPhone: '',
+      validate: {
+        nameState: '',
+        emailState: '',
+        celphoneState: ''
+      }
+    };
+    
+    this.handleChanges = this.handleChanges.bind(this);
+  };
+
+  handleChanges = (event) => {
+    const { target } = event;
+    const value = target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateFields(e) {
+    const emailRegex =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const nameRegex =  /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+    const celRegex = /^\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}$/; // matches mask (99) 99999-9999.
+
+    const { validate } = this.state;
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    switch(fieldName) {
+      case 'clientName':
+        validate.nameState = 'has-danger';  
+        
+        if( nameRegex.test(fieldValue) ) {
+          validate.nameState = 'has-success';
+        }
+        break;
+      case 'clientEmail':
+        validate.emailState = 'has-danger';
+        
+        if( emailRegex.test(fieldValue) ) {
+          validate.emailState = 'has-success';
+        }
+        break;
+      case 'clientPhone':
+        validate.celphoneState = 'has-danger';
+        
+        if( celRegex.test(fieldValue) ) {
+          validate.celphoneState = 'has-success';
+        }
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ validate });
+  }
+
+  sendEmail(e) {
     debugger;
-    console.log('service id ==> ', process.env.REACT_APP_SERVICE_ID);
     e.preventDefault();
 
     emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,
@@ -23,27 +87,67 @@ const FormComponent = () => {
       });
   }
 
-  return(
+  render() {
+    const { clientName, clientEmail, clientPhone } = this.state;
+    return(
       <div className="form-container">
-        <Form onSubmit={sendEmail} >
-        <FormGroup className="field-size">
-            <Label for="nomeField" className="field-title">Nome:</Label>
-            <Input type="text" name="nome_cliente" id="nomeField" placeholder="nome completo" />
+        <Form>
+          <FormGroup className="field-size">
+            <Label for="nameField" className="field-title">Nome:</Label>
+            <Input 
+              type="text" 
+              name="clientName" 
+              id="nameField" 
+              placeholder="nome completo"
+              valid={ this.state.validate.nameState === 'has-success' }
+              invalid={ this.state.validate.nameState === 'has-danger' }
+              value={ clientName }
+              onChange={ (e) => {
+                this.handleChanges(e);
+              }}
+              onBlur={ (e) => {
+                this.validateFields(e);
+              }} />
+            <FormFeedback>Esse campo não aceita números e caracteres especiais.</FormFeedback>
           </FormGroup>
           <FormGroup className="field-size">
             <Label for="emailField" className="field-title">E-mail:</Label>
-            <Input type="email" name="email_cliente" id="emailField" placeholder="insira seu email" />
+            <Input 
+              type="email" 
+              name="clientEmail" 
+              id="emailField" 
+              placeholder="insira seu email"
+              valid={ this.state.validate.emailState === 'has-success' }
+              invalid={ this.state.validate.emailState === 'has-danger' }
+              value={ clientEmail }
+              onChange={ (e) => {
+                this.handleChanges(e);
+              }}
+              onBlur={ (e) => {
+                this.validateFields(e);
+              }} />
+            <FormFeedback>Desculpe, e-mail invalido.</FormFeedback>
           </FormGroup>
           <FormGroup className="field-size">
             <Label for="celField" className="field-title">Celular:</Label>
             <Input 
               type="cel" 
-              name="cel_cliente" 
+              name="clientPhone" 
               id="celField" 
               mask="(99) 99999-9999" 
               maskChar=" " 
               tag={InputMask}
-              placeholder="(11) 99999-9999" />
+              placeholder="(11) 99999-9999"
+              valid={ this.state.validate.celphoneState === 'has-success' }
+              invalid={ this.state.validate.celphoneState === 'has-danger' }
+              value={ clientPhone }
+              onChange={ (e) => {
+                this.handleChanges(e);
+              }}
+              onBlur={ (e) => {
+                this.validateFields(e);
+              }} />
+            <FormFeedback>Desculpe, celular inválido. Preencha conforme (99) 99999-9999.</FormFeedback>
           </FormGroup>
           <FormGroup>
             <Label for="msgField" className="field-title">Mensagem:</Label>
@@ -52,8 +156,8 @@ const FormComponent = () => {
           <Button type="submit" style={{ marginTop: '2em'}}>Enviar</Button>
         </Form>
       </div>
-
-  )
+    );
+  }
 }
 
 export default FormComponent;
